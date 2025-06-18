d5d6c4cc98b810c4bceef5bc3bb3063324c4c9ca
Dennis Lindo <d.lindo@gmail.com>
2025-06-18 16:59:03 -0400

    trajan font added to h2 and h3

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        trajan: ["TrajanFont", "serif"],
+      },
+    },
+  },
+  plugins: [],
+}
