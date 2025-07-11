c3eb2db1dce654a4393d4bfb5814e1b2f226b173
Dennis Lindo <d.lindo@gmail.com>
2025-07-11 13:43:37 -0400

    contact email styled

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -45,12 +45,97 @@ export const POST: APIRoute = async ({ request }) => {
       to: import.meta.env.CONTACT_RECIPIENT_EMAIL,
       subject: `New message from ${name || email}`,
       html: `
-        <p><strong>Name:</strong> ${name || '—'}</p>
-        <p><strong>Telephone:</strong> ${phone || '—'}</p>
-        <p><strong>Email:</strong> ${email}</p>
-        <hr>
-        <p>${message.replace(/\n/g, '<br>')}</p>
-      `,
+        <div
+  style="
+    background-color: #f5f5f4;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  "
+>
+  <div
+    style="
+      width: 91.666667%;
+      max-width: 700px;
+      margin: auto;
+      background-color: #ffffff;
+      padding: 3rem;
+      border-radius: 0.75rem;
+      box-shadow: 5px 10px 18px #888888;
+      position: relative;
+      margin-top: 3rem;
+      margin-bottom: 3rem;
+    "
+  >
+    <p
+      style="
+        line-height: 2;
+        text-align: left;
+        font-size: 2rem;
+        font-family: ui-serif, Georgia, Cambria, 'Times New Roman', Times, serif;
+        color: #c3003c;
+        max-width: 600px;
+      "
+    >
+NEW MESSAGE FROM WEBSITE:    </p>    
+    <p
+      style="
+        line-height: 2;
+        text-align: justify;
+        font-size: 1.25rem;
+        font-family: ui-serif, Georgia, Cambria, 'Times New Roman', Times, serif;
+        color: #0c4a6e;
+        max-width: 600px;
+      "
+    >
+     ${message.replace(/\n/g, '<br>')}
+    </p>
+    <hr
+      style="
+        width: 83.333333%;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 2.5rem;
+        margin-bottom: 2.5rem;
+        border: none;
+        border-top: 1px solid #c3003c;
+      "
+    />
+    <table
+  style="
+    width: 100%;
+    max-width: 700px;
+    margin: auto;
+    border-collapse: collapse;
+    font-size: 1.25rem;
+    font-family: ui-serif, Georgia, Cambria, 'Times New Roman', Times, serif;
+    color: #0c4a6e;
+  "
+>
+  <tbody>
+    <tr>
+      <td style="padding: 0.5rem 0.5rem;">Sender Name:</td>
+      <td style="padding: 0.5rem 0.5rem;">${name || '—'}</td>
+    </tr>
+    <tr>
+      <td style="padding: 0.5rem 0.5rem;">Sender Email:</td>
+      <td style="padding: 0.5rem 0.5rem;">
+        <a href="mailto:${email}">${email}</a>
+      </td>
+    </tr>
+    <tr>
+      <td style="padding: 0.5rem 0.5rem;">Sender Phone:</td>
+      <td style="padding: 0.5rem 0.5rem;">${phone || '—'}</td>
+    </tr>
+  </tbody>
+</table>
+  </div>
+</div>`,
     });
 
     return new Response(
